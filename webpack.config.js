8a55ce1fa0c126b6dbf82f223dec917d6724c229
Severiano Badajoz <sevebadajoz@gmail.com>
2019-01-14 10:19:25 -0800

    Create webpack config

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+
+const path = require('path');
+
+module.exports = {
+  entry: './js/index.js',
+  output: {
+    path: path.resolve(__dirname, './js'),
+    filename: 'main-bundled.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.hbs$/,
+        use: [{
+          loader: 'handlebars-loader',
+          options: { helperDirs: path.resolve(__dirname, './js/helpers') },
+        }],
+      },
+    ],
+  },
+};
